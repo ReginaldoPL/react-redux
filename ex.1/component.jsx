4e327747d9736874a85bd29b66f4ec302f75f7ef
Reginaldo <reginaldo.pedro.de.lima@gmail.com>
2018-05-27 14:19:33 -0300

    first commit

---
+++ ex.1/component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default const Primeiro = props => (
+    <h1>Primeiro Componente!</h1>
+)
+
+export const Segundo = props =>  <h1>Segundo Componente!</h1>
+
+//export {Primeiro,Segundo}
